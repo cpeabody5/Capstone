c98c26c24e9991432407096236d4cddaca5430a8
zainosaurus <zain.tahlilkar@gmail.com>
2020-01-22 11:07:50 -0500

    Moving Average

--- fft_py/analyze.py
+++ fft_py/analyze.py
@@ -9,6 +9,7 @@ Required:
 '''
 
 import numpy as np
+from scipy import ndimage
 import sys
 import time
 import matplotlib.pyplot as plt
@@ -23,7 +24,7 @@ class AudioAnalyzer:
     # For windowing (sliding window)
     WIN_DURATION = 0.1  # in seconds
     STEP_SIZE = 0.1     # in seconds
-
+    
 
     def plot(self, x1, y1, x2, y2, x3, y3, filename='Audio File', hold=True):
         plt.gcf().clear()
@@ -75,11 +76,35 @@ class AudioAnalyzer:
         else:
             window = self.audio.get_window(i, self.WIN_DURATION, self.STEP_SIZE)
             audio_len = self.WIN_DURATION   # in seconds
-        print("reached 80, window.any() = {}".format(window.any()))
+
+        freq_accum = None        
         while window.any():
             # Perform fft
             freq = self.get_frequencies(window)
             freq_db = self.convert_to_db(freq)
+
+            # Preprocess
+            # standarization
+            mean_freq = np.average(freq)
+            std_freq = np.std(freq)
+            freq = (freq - mean_freq)/std_freq
+
+            # Gaussian Filter
+            #freq = ndimage.gaussian_filter1d(freq,1)
+            
+            # Accumulate frequencies
+            if freq_accum is None:
+                freq_accum = np.expand_dims(freq,0) 
+            else:
+                if not np.all(freq == freq_accum[-1]):
+                    freq_accum = np.concatenate((freq_accum, np.expand_dims(freq,0)), axis = 0)
+                    freq_accum = freq_accum[-10:]
+
+            weights = np.arange(len(freq_accum))+1
+            weights =[np.power(weights,3)]*len(freq_accum[0])
+            weights = np.transpose(weights)
+            weights = weights/np.max(weights)
+            freq_db = np.average(freq_accum,0,weights=weights)
 
             # Signal/Time plot
             if self.realtime:
@@ -119,6 +144,8 @@ class AudioAnalyzer:
             self.realtime = True
 
         self.analyze()
+
+
 
 # Run Program
 AudioAnalyzer()
